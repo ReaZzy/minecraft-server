e371ea1ce038f740f4e41a5c2550634ea684302b
Maksym <maksym.nebela@praguelabs.com>
2022-07-22 23:17:44 +0200

    feat: server_save

--- start-server-1gb-ram.sh
+++ start-server-1gb-ram.sh
@@ -5,7 +5,7 @@ BOLDGREEN="\e[1;${GREEN}m"
 ENDCOLOR="\e[0m"
 
 function closeServer () {
-  read -p '\n\n${BOLDGREEN}Назва збереження:${ENDCOLOR} ' saveName
+  read -p "\n\n${BOLDGREEN}Назва збереження:${ENDCOLOR} " saveName
   saveName=${saveName:-server_save}
   git pull origin master && \
   git add . && \
