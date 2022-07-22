24fd941f024315fcea5a338475b1b623bdde7ebf
Maksym <maksym.nebela@praguelabs.com>
2022-07-22 23:11:29 +0200

    feat:

--- start-server-1gb-ram.sh
+++ start-server-1gb-ram.sh
@@ -10,4 +10,4 @@ function closeServer () {
 
 git pull origin master && \
 java -Xmx1024M -Xms1024M -jar server.jar nogui
-trap "git pull origin master && git add . && git commit -am \"feat: $saveName\" && git push origin master" INT SIGTERM SIGINT exit
+trap "git add . && git commit -am \"feat: $saveName\" && git push origin master" INT SIGTERM SIGINT exit
