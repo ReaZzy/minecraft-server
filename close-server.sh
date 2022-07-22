3cb126b434e245e2841a8e5fbb978dfbe0768701
Maksym <maksym.nebela@praguelabs.com>
2022-07-22 22:51:27 +0200

    feat: server_save

--- close-server.sh
+++ close-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+set -e
 read -p 'Назва збереження: ' saveName
 saveName=${saveName:-server_save}
 git pull origin master && \
 git add . && \
-git commit -am "feat: $saveName" && \
-git push -u origin master
+git commit -am "feat: $saveName" && git push -u origin master
